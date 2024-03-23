1a58742b33d19c43b3c79b5a2f55e1daf2e6706e
maham <Mahishaikh2428@gmail.com>
2024-03-23 00:58:28 -0700

    basic program

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+//
+
+const ranNum = Math.floor (Math.random()*6+1);
+//console.log(ranNum);
+const numguess = await inquirer.prompt([
+  {
+    name: "UserNumber",
+    type: "number",
+    message: "Please guess any number b/w 1 ~ 6:    ",
+  },
+]);
+ if (numguess.UserNumber === ranNum) {
+  console.log(
+    "Congragulations!, you guessed ",
+    numguess.UserNumber,
+    " which is correct"
+  );
+} else {
+   console.log(
+   "Sorry you guessed it wrong, my supposition: ",
+    ranNum,
+    ", but you guessed: ",
+    numguess.UserNumber
+  );
+}
